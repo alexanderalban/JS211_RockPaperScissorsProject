cb073596ab22edcd6bb8114aee049043bd0f6379
alexanderalban <albanalexander@gmail.com>
2020-10-26 21:58:39 -0500

    images now change with inputs

--- main.js
+++ main.js
@@ -18,26 +18,35 @@ hand2 = document.getElementById("p2-selection").value.toLowerCase().trim();
     document.getElementById("display-result").innerHTML = "It's a tie!"
     return "It's a tie!"
   } else if (hand1 === "rock") {
+    document.getElementById("left-hand").src ="./images/hand-rock.png";
     if (hand2 === "scissors") {
+      document.getElementById("right-hand").src ="./images/hand-scissors-right.png";
       document.getElementById("display-result").innerHTML = "Player 1 Wins!"
       return "Hand one wins!"
     } else if (hand2 = "paper") {
+      document.getElementById("right-hand").src ="./images/hand-paper-right.png";
       document.getElementById("display-result").innerHTML = "Player 2 Wins!"
       return "Hand two wins!"
     }
   } else if (hand1 === "paper") {
+    document.getElementById("left-hand").src ="./images/hand-paper-left.png";
     if (hand2 === "rock") {
+      document.getElementById("right-hand").src ="./images/hand-rock-player-two.png";
       document.getElementById("display-result").innerHTML = "Player 1 Wins!"
       return "Hand one wins!"
     } else if (hand2 === "scissors") {
+      document.getElementById("right-hand").src ="./images/hand-scissors-right.png";
       document.getElementById("display-result").innerHTML = "Player 2 Wins!"
       return "Hand two wins!"
     }
   } else if (hand1 === "scissors") {
+    document.getElementById("left-hand").src ="./images/hand-scissors-left.png";
     if (hand2 === "paper") {
+      document.getElementById("right-hand").src ="./images/hand-paper-right.png";
       document.getElementById("display-result").innerHTML = "Player 1 Wins!"
       return "Hand one wins!"
     } else if (hand2 = "rock") {
+      document.getElementById("right-hand").src ="./images/hand-rock-player-two.png";
       document.getElementById("display-result").innerHTML = "Player 2 Wins!"
       return "Hand two wins!"
     }
